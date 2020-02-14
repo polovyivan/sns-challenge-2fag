70431ea930a4ae42b5d942d6568b573225c552af
Ivan <polovyiivan@gmail.com>
2020-02-14 09:43:26 -0300

    initial setup

---
+++ src/main/java/com/ivan/polovyi/challenge/sns/fag/snschallenge2fag/SnsChallenge2fagApplication.java
@@ -0,0 +1,13 @@
+package com.ivan.polovyi.challenge.sns.fag.snschallenge2fag;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SnsChallenge2fagApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SnsChallenge2fagApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/ivan/polovyi/challenge/sns/fag/snschallenge2fag/SnsChallenge2fagApplicationTests.java
@@ -0,0 +1,13 @@
+package com.ivan.polovyi.challenge.sns.fag.snschallenge2fag;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SnsChallenge2fagApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
